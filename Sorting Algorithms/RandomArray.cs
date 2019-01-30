d1d8b4e9cb0611b6617b738121ef68544df31cc2
Matt <armada2526pbem@gmail.com>
2019-01-30 12:53:37 -0700

    Initial commit
    Copied project files into git repo folder. Currently, program is able to bubble sort. Also has "visual" bubble sort where it just prints the array as it makes changes.

---
+++ Sorting Algorithms/RandomArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bubble_Sort
+{
+    class RandomArray
+    {
+        //Generates a list of integers of a given size, with a given max integer value
+        public static List<int> genIntList(int size, int maxInt)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(new Random().Next(maxInt));
+            }
+
+            return list;
+        }
+
+
+    }
+}
---
+++ Sorting Algorithms/SortingAlgorithms.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bubble_Sort
+{
+    class SortingAlgorithms
+    {
+        // Prints out an array in format [ 0 1 2 ] and so forth
+        public static void printArray(int[] arrayToPrint)
+        {
+            Console.Write("[ ");
+            foreach (int i in arrayToPrint)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine("]");
+        }
+        // Visually sorts an array of integers using the Bubble Sort algorithm
+        public static void visualBubbleSort(int[] arrayToSort)
+        {
+            printArray(arrayToSort);
+            for (int i = 0; i + 1 < arrayToSort.Length; i++)
+            {
+                if (arrayToSort[i] > arrayToSort[i + 1])
+                {
+                    int temp = arrayToSort[i + 1];
+                    arrayToSort[i + 1] = arrayToSort[i];
+                    arrayToSort[i] = temp;
+                    i = -1;
+                    printArray(arrayToSort);
+                }
+            }
+            printArray(arrayToSort);
+
+        }
+        // Returns a sorted list, using the Bubble Sort algorithm
+        public static int[] bubbleSort(int[] arrayToSort)
+        {
+            for (int i = 0; i + 1 < arrayToSort.Length; i++)
+            {
+                if (arrayToSort[i] > arrayToSort[i + 1])
+                {
+                    int temp = arrayToSort[i + 1];
+                    arrayToSort[i + 1] = arrayToSort[i];
+                    arrayToSort[i] = temp;
+                    i = -1;
+                }
+            }
+            return arrayToSort;
+        }
+    }
+}
